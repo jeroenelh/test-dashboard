7fe045b8300a35bfc105a0d8f27e38e60e9fa64b
Jeroen el Hahaoui <jeroen.elhahaoui@zibber.nl>
2026-06-17 01:11:22 +0200

    adjust date range in DashboardController: extend default data retrieval window from 10 days to 30 days and set timeout limit

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -49,9 +49,10 @@ class DashboardController
 
     public function v8(Request $request)
     {
+        set_time_limit(60);
         $this->setDatabase();
         $country  = $request->get('country', 'ALL');
-        $dateFrom = now()->subDays(10)->startOfDay()->toDateTimeString();
+        $dateFrom = now()->subDays(30)->startOfDay()->toDateTimeString();
         $dateTo   = now()->toDateTimeString();
         $cacheKey = 'dashboard_v8_' . md5($country . date('Y-m-d-H'));
 
